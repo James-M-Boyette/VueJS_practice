b8ada8633dc11ad18ef7d269628a99a859790780
James Boyette <james.boyette@gmail.com>
2021-03-21 12:56:02 -0400

    Add Axios call, response and display

---
+++ webrequests/webrequests.js
@@ -0,0 +1,34 @@
+/* global Vue, axios */axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Making Web Requests (A Practice File)",
+    todos: [],
+    newTodo:"",
+  },
+  methods: {
+    // ...
+    loadTodos: function() { 
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then(response => {
+          console.log(response.data);
+          this.todos = response.data;
+        })
+    },
+    makeNewTodo: function(){
+      console.log('post request');
+      axios.post("https://jsonplaceholder.typicode.com/todos", {
+        userId: 4,
+        title: this.newTodo,
+      }).then(response => {console.log(response);
+      this.todos.push(response.data);
+    });
+    },
+  },
+  created: function(){
+    console.log('in created...');
+    this.loadTodos();
+  },
+});
